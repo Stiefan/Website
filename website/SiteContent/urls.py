9412a8283fb603f9084a87cd62e1659706eb550e
Stiefan <s.j.marges@gmail.com>
2015-11-10 09:26:04 +0100

    urls gefixed

--- website/SiteContent/urls.py
+++ website/SiteContent/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import include, url
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.SiteContent, name='SiteContent'),
+    url(r'^about', views.About, name='about'),
+    url(r'^contact', views.Contact, name='contact'),
 ]
--- website/SiteContent/views.py
+++ website/SiteContent/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 # Create your views here.
 def SiteContent(request):
 	return render(request, 'SiteContent/index.html')
+
+def About(request):
+	return render(request, 'SiteContent/About.html')
+
+def Contact(request):
+	return render(request, 'SiteContent/contact.html')
